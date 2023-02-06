58e87010fca136ffcf1da8b537bb6ed8f074c835
Jagroshan <jagroshansingh615@gmail.com>
2023-02-07 02:17:40 +0530

    price slider functionality working

--- my-app/src/Components/SearchPanel.jsx
+++ my-app/src/Components/SearchPanel.jsx
@@ -1,87 +1,122 @@
-import { Button, Stack, Icon, Input, InputGroup, InputLeftElement, InputRightElement, Tab, TabList, TabPanel, TabPanels, Tabs, useColorModeValue, InputLeftAddon, Box, HStack, useToast, Text } from "@chakra-ui/react"
-import { ImLocation, ImCalendar } from 'react-icons/im'
-import { Link, useNavigate } from "react-router-dom"
-import DatalistInput from 'react-datalist-input';
-import 'react-datalist-input/dist/styles.css';
+import {
+  Button,
+  Stack,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  useColorModeValue,
+  InputLeftAddon,
+  Box,
+  HStack,
+  useToast,
+  Text,
+} from "@chakra-ui/react";
+import { ImLocation, ImCalendar } from "react-icons/im";
+import { Link, useNavigate } from "react-router-dom";
+import DatalistInput from "react-datalist-input";
+import "react-datalist-input/dist/styles.css";
 import { useContext } from "react";
 import { SearchContext } from "../Contexts/SearchContextProvider";
 import { useState } from "react";
-import Alert from './Alert'
-
-
+import Alert from "./Alert";
 
 export default function SearchPanel() {
-    const navigate = useNavigate();
-    const { setsearch } = useContext(SearchContext)
-    const [location, setlocation] = useState(null)
-    const toast = useToast()
-    let alertdata={
-        title: ' Invalid Input',
-        description: "Please check the input again",
-        status: 'warning',
+  const navigate = useNavigate();
+  const { setsearch } = useContext(SearchContext);
+  const [location, setlocation] = useState(null);
+  const toast = useToast();
+  let alertdata = {
+    title: " Invalid Input",
+    description: "Please check the input again",
+    status: "warning",
+  };
+
+  let initialdata = {
+    destination: null,
+    checkin: null,
+    checkout: null,
+    travellers: null,
+    rooms: null,
+  };
+
+  const [traveldata, settraveldata] = useState(initialdata);
+  //console.log(traveldata)
+
+  let handletraveller = (el) => {
+    settraveldata({ ...traveldata, [el.target.name]: el.target.value });
+  };
+
+  const handleSearch = () => {
+    let flag = true;
+    for (let key in traveldata) {
+      if (traveldata[key] == null) {
+        toast(Alert(alertdata));
+        flag = false;
+        break;
       }
-
-    let initialdata = {
-        destination: null,
-        checkin: null,
-        checkout: null,
-        travellers: null,
-        rooms: null,
+    }
+    if (
+      traveldata.travellers < traveldata.rooms ||
+      traveldata.travellers / traveldata.rooms > 3
+    ) {
+      toast(Alert(alertdata));
+      flag = false;
     }
 
-    const [traveldata, settraveldata] = useState(initialdata)
-    //console.log(traveldata)
-
-    let handletraveller = (el) => {
-        settraveldata({ ...traveldata, [el.target.name]: el.target.value })
+    if (flag == true) {
+      let bookingdata = JSON.parse(localStorage.getItem("booking"));
+      setsearch(location);
+      navigate("/products");
+      localStorage.setItem(
+        "booking",
+        JSON.stringify({ ...bookingdata, ...traveldata })
+      );
     }
+  };
 
-    const handleSearch = () => {
-        let flag=true
-        for(let key in traveldata)
-        {
-            if(traveldata[key]==null)
-            { 
-                toast(Alert(alertdata)) 
-                flag=false;
-                break;
-            }
-        }
-        if (traveldata.travellers < traveldata.rooms || traveldata.travellers/traveldata.rooms>3)
-        { 
-            toast(Alert(alertdata))
-            flag=false;
-        }
-
-        if(flag==true) 
-        {
-            let bookingdata=JSON.parse(localStorage.getItem('booking'))
-            setsearch(location)
-            navigate('/products')
-            localStorage.setItem('booking',JSON.stringify({...bookingdata,...traveldata}))
-        }
-    }
-
-    return <div style={{ 'marginBottom': '2%','border':'0px solid' }}>
-        <Box marginTop='2%'>
-            <InputGroup w='20em' margin='auto'>
-                <InputLeftAddon>Going to</InputLeftAddon>
-                <DatalistInput
-                    
-                    placeholder="Enter the destination"
-                    onSelect={(item) => { setlocation(item.value); traveldata.destination = item.value }}
-                    items={[
-                        { id: 'Antartica', value: 'Antartica' },
-                        { id: 'Shara Desert', value: 'Shara Desert' },
-                        { id: 'Amazon', value: 'Amazon' },
-                        { id: 'Mt.Everest', value: 'Mt.Everest' },
-                        { id: 'Goa', value: 'Goa' },
-                    ]}
-                />
-            </InputGroup>
-        </Box>
-        <Stack direction={{ base: 'column', lg: 'row' }} spacing={2} padding={5} border='0px solid' justifyContent='space-evenly'>
-            {/* <InputGroup>
+  return (
+    <div style={{ marginBottom: "2%", border: "0px solid" }}>
+      <Box marginTop="2%">
+        <InputGroup
+          color={"gray.500"}
+          borderRadius={"0%"}
+          border={"0.5rem outset pink"}
+          w={{ base: "14em", sm: "20em" }}
+          margin="auto"
+        >
+          <InputLeftAddon h={"auto"}>Going to</InputLeftAddon>
+          <DatalistInput
+            // border={'1px'}
+            placeholder="Enter the destination"
+            onSelect={(item) => {
+              setlocation(item.value);
+              traveldata.destination = item.value;
+            }}
+            items={[
+              { id: "Antartica", value: "Antartica" },
+              { id: "Shara Desert", value: "Shara Desert" },
+              { id: "Amazon", value: "Amazon" },
+              { id: "Mt.Everest", value: "Mt.Everest" },
+              { id: "Goa", value: "Goa" },
+            ]}
+          />
+        </InputGroup>
+      </Box>
+      <Stack
+        direction={{ base: "column", lg: "row" }}
+        spacing={2}
+        padding={5}
+        border="0px solid"
+        justifyContent="space-evenly"
+      >
+        {/* <InputGroup>
                 <InputLeftElement
                     color='gray.400'
                     fontSize='1.2em'
@@ -89,61 +124,60 @@ export default function SearchPanel() {
                 />              
                 <Input placeholder="Going to"/>
             </InputGroup> */}
-            <Stack direction={{ base: 'column', sm: 'row' }}>
-                <InputGroup>
-                    <InputLeftAddon>Check In:</InputLeftAddon>
-                    <Input
-                        //placeholder="Enter the date"
-                        size="md"
-                        type="date"
-                        name="checkin"
-                        onChange={handletraveller}
-                    //onFocus={(el) => el.target.type = 'date'}
-                    />
-                </InputGroup>
+        <Stack direction={{ base: "column", sm: "row" }}>
+          <InputGroup>
+            <InputLeftAddon>Check In</InputLeftAddon>
+            <Input
+              size="md"
+              type="date"
+              name="checkin"
+              onChange={handletraveller}
+              //onFocus={(el) => el.target.type = 'date'}
+            />
+          </InputGroup>
 
-                <InputGroup>
-                    <InputLeftAddon>Check Out:</InputLeftAddon>
-                    <Input
-                        //placeholder="Entet the date"
-                        size="md"
-                        type="date"
-                        name="checkout"
-                        onChange={handletraveller}
-                    //onFocus={(el) => el.target.type = 'date'}
-                    />
-                </InputGroup>
-            </Stack>
-            <Stack direction={{ base: 'column', sm: 'row' }}>
-                <InputGroup>
-                    <InputLeftAddon>Travellers</InputLeftAddon>
-                    <Input
-                        placeholder="Number of Travellers"
-                        size="md"
-                        type="number"
-                        name="travellers"
-                        onChange={handletraveller}
-                    />
-                </InputGroup>
-
-                <InputGroup>
-                    <InputLeftAddon>Rooms</InputLeftAddon>
-                    <Input
-                        placeholder="Number of Rooms"
-                        size="md"
-                        type="number"
-                        name="rooms"
-                        onChange={handletraveller}
-                    />
-                </InputGroup>
-            </Stack>
+          <InputGroup>
+            <InputLeftAddon>Check Out</InputLeftAddon>
+            <Input
+              //placeholder="Entet the date"
+              size="md"
+              type="date"
+              name="checkout"
+              onChange={handletraveller}
+              //onFocus={(el) => el.target.type = 'date'}
+            />
+          </InputGroup>
         </Stack>
+        <Stack direction={{ base: "column", sm: "row" }}>
+          <InputGroup>
+            <InputLeftAddon>Travellers</InputLeftAddon>
+            <Input
+              placeholder="Number of Travellers"
+              size="md"
+              type="number"
+              name="travellers"
+              onChange={handletraveller}
+            />
+          </InputGroup>
 
-        <Button colorScheme='blue' size='lg' onClick={handleSearch}>
-            Search
-        </Button>
+          <InputGroup>
+            <InputLeftAddon>Rooms</InputLeftAddon>
+            <Input
+              placeholder="Number of Rooms"
+              size="md"
+              type="number"
+              name="rooms"
+              onChange={handletraveller}
+            />
+          </InputGroup>
+        </Stack>
+      </Stack>
 
-        <Text textAlign='end'>*Maximum:  3 Individuals/room</Text>
+      <Button colorScheme="blue" size="lg" onClick={handleSearch}>
+        Search
+      </Button>
 
+      <Text textAlign="end">*Maximum: 3 Individuals/room</Text>
     </div>
+  );
 }
--- my-app/src/Pages/ProductsPage.jsx
+++ my-app/src/Pages/ProductsPage.jsx
@@ -1,4 +1,20 @@
-import { AspectRatio, Box, Checkbox, Container, Divider, Heading, Hide, Input, Link, SkeletonCircle, SkeletonText, Stack, Text, Tooltip, VStack } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  Checkbox,
+  Container,
+  Divider,
+  Heading,
+  Hide,
+  Input,
+  Link,
+  SkeletonCircle,
+  SkeletonText,
+  Stack,
+  Text,
+  Tooltip,
+  VStack,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useContext, useEffect } from "react";
 import { useState } from "react";
@@ -7,82 +23,112 @@ import ProductCard from "../Components/ProductCard";
 import SearchPanel from "../Components/SearchPanel";
 import { SearchContext } from "../Contexts/SearchContextProvider";
 
-
 export default function ProductsPage() {
-    const { search } = useContext(SearchContext)
-    const [products, setproducts] = useState([])
-    useEffect(() => {
-        const FtchData = async () => {
-            try {
-                let res = await axios({
-                    method: 'get',
-                    url: `http://localhost:3000/products?location=${search}`,
-                })
-                //https://real-rose-tortoise-tutu.cyclic.app/products
-                //console.log(res)
-                setproducts(res.data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        FtchData()
-    }, [search])
+  const { search } = useContext(SearchContext);
+  const [products, setproducts] = useState([]);
+  const [price, setprice] = useState(0);
+  useEffect(() => {
+    const FtchData = async () => {
+      try {
+        let res = await axios({
+          method: "get",
+          url: `http://localhost:3000/products?location=${search}`,
+        });
+        //https://real-rose-tortoise-tutu.cyclic.app/products
+        // console.log(res)
+        let priceFiltered=res.data.filter((prod)=>prod.price<=price)     
+        setproducts(priceFiltered);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    FtchData();
+  }, [search,price]);
 
+//   useEffect(()=>{
+//     // console.log(products.map((product)=>product))
+//     // setproducts(products.filter((prod)=>prod.price<=price))
+//   },[price])
 
+  return (
+    <Box px={10}>
+      <SearchPanel />
 
-    return <Box px={10}>
-        <SearchPanel />
+      <Stack direction="row" spacing={4}>
+        <Hide below="md">
+          <VStack
+            w={{ sm: "0%", md: "30%" }}
+            border="0px solid grey"
+            align="flex-start"
+          >
+            <Heading size="md">Traveller Experience</Heading>
+            <Checkbox size="lg">LGBTQ welcoming:</Checkbox>
+            <Text paddingLeft={6} textAlign="start">
+              See properties that pledge to make all guests feel safe, welcome,
+              and respected.
+            </Text>
 
-        <Stack direction='row' spacing={4}>
-            <Hide below='md'>
-                <VStack w={{ sm: '0%', md: '30%' }} border='0px solid grey' align='flex-start'>
-                    <Heading size='md'>Traveller Experience</Heading>
-                    <Checkbox size='lg'>
-                        LGBTQ welcoming:
-                    </Checkbox>
-                    <Text paddingLeft={6} textAlign='start'>See properties that pledge to make all guests feel safe, welcome, and respected.</Text>
+            <Checkbox size="lg">Wheel-Chair Frendly:</Checkbox>
+            <Text paddingLeft={6} textAlign="start">
+              See properties that pledge to make all guests feel safe, welcome,
+              and respected.
+            </Text>
 
-                    <Checkbox size='lg'>
-                        Wheel-Chair Frendly:
-                    </Checkbox>
-                    <Text paddingLeft={6} textAlign='start'>See properties that pledge to make all guests feel safe, welcome, and respected.</Text>
+            <Checkbox size="lg">Family Friendly:</Checkbox>
+            <Text paddingLeft={6} textAlign="start">
+              See properties that pledge to make all guests feel safe, welcome,
+              and respected.
+            </Text>
+            <Divider orientation="horizontal" />
+            <br />
+            <Heading size="md">Price</Heading>
+            <Container direction="row">
+              <PriceSlider setprice={setprice} />
+            </Container>
+          </VStack>
+        </Hide>
 
-                    <Checkbox size='lg'>
-                        Family Friendly:
-                    </Checkbox>
-                    <Text paddingLeft={6} textAlign='start'>See properties that pledge to make all guests feel safe, welcome, and respected.</Text>
-                    <Divider orientation='horizontal' />
-                    <br />
-                    <Heading size='md'>Price</Heading>
-                    <Container direction='row'><PriceSlider /></Container>
-                </VStack>
-            </Hide>
-            
-                {products.length == 0 ? <Box w='100%'><Box padding='6' boxShadow='lg' bg='white'>
-                    <SkeletonCircle size='10' />
-                    <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-                </Box>
-                    <Box padding='6' boxShadow='lg' bg='white'>
-                        <SkeletonCircle size='10' />
-                        <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-                    </Box>
-                    <Box padding='6' boxShadow='lg' bg='white'>
-                        <SkeletonCircle size='10' />
-                        <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-                    </Box>
-                </Box>
-                    :
-
-                    <VStack w={{ base: '100%', sm: '100%', md: '70%' }} border='0px solid'>
-
-                        {products.map((product) => <ProductCard product={product} key={product.id} id={product.id} />)}
-
-                    </VStack>
-                }
-        
-        </Stack>
-
-
+        {products.length == 0 ? (
+          <Box w="100%">
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+          </Box>
+        ) : (
+          <VStack
+            w={{ base: "100%", sm: "100%", md: "70%" }}
+            border="0px solid"
+          >
+            {products.map((product) => (
+              <ProductCard product={product} key={product.id} id={product.id} />
+            ))}
+          </VStack>
+        )}
+      </Stack>
     </Box>
-
+  );
 }
