fa21d24c8e425acf2f271b1fdff86aab934576ab
Jagroshan <jagroshansingh615@gmail.com>
2023-03-22 11:55:35 +0530

    romoved commented extra codes

--- my-app/src/Components/Alert.jsx
+++ my-app/src/Components/Alert.jsx
@@ -1,8 +1,6 @@
 
-
 export default function Alert(prop) {
         const {title,description,status}=prop
-        //console.log(title)
         return {
                 title: title,
                 description: description,
@@ -12,14 +10,4 @@ export default function Alert(prop) {
                 position:'top'
               }
 }
-
-/*const toast = useToast()
-    let alertdata={
-        title: ' Payment Declined',
-        description: "Bank server is not responding",
-        status: 'error',
-      }
-
- toast(Alert(alertdata))
-*/      
         
