41fa7b6e3430c02bb3e26d314e74a5308f8826f1
Jagroshan <jagroshansingh615@gmail.com>
2023-05-28 15:09:08 +0530

    centered the Product and User button of Admin

--- Frontend/src/Pages/Admin/Admin.jsx
+++ Frontend/src/Pages/Admin/Admin.jsx
@@ -1,15 +1,21 @@
-import { Box, Button } from '@chakra-ui/react'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Box, Button } from "@chakra-ui/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Admin = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <div>
-      <Box h={'100vh'} >
-      <Button onClick={()=>navigate('/admin/productDashboard')}>Product Dashboard</Button>
-      <Button onClick={()=>navigate('/admin/userDashboard')}>User Dashboard</Button>
+      <Box h={"63vh"} border={"0px"} display={"grid"} alignItems={"center"}>
+        <Box border={"0px"} display={"flex"} justifyContent={"space-evenly"}>
+          <Button onClick={() => navigate("/admin/productDashboard")}>
+            Product Dashboard
+          </Button>
+          <Button onClick={() => navigate("/admin/userDashboard")}>
+            User Dashboard
+          </Button>
+        </Box>
       </Box>
     </div>
-  )
-}
+  );
+};
